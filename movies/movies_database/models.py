ed6a4449c8b0cd825f2027c62dba0d26a31351bd
yourdrug <mitkojenia@gmail.com>
2023-09-14 20:18:13 +0300

    Add 'owner' to Movie model

--- movies/movies_database/models.py
+++ movies/movies_database/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -5,6 +6,7 @@ class Movie(models.Model):
     name = models.CharField(max_length=80)
     year = models.SmallIntegerField()
     country = models.CharField(max_length=80)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f'Id {self.id}: {self.name} {self.year}'
--- movies/movies_database/views.py
+++ movies/movies_database/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
 from movies_database.models import Movie
@@ -11,8 +11,8 @@ from movies_database.serializers import MovieSerializer
 class MovieViewSet(ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
-    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     filter_fields = ['year', 'name']
     search_fields = ['name', 'country']
 
