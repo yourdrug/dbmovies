a792a3c7b464b24be148e9a5056955691cb4555b
yourdrug <mitkojenia@gmail.com>
2023-10-10 18:21:19 +0300

    create client

---
+++ ui/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div v-for="movie in movies" :key="movie.id">
+      <div>Title {{ movie.name }}</div>
+      <img v-bind:src="movie.poster" class="photo" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      movies: [],
+    };
+  },
+
+  created() {
+    fetch("http://127.0.0.1:8000/movie/", {
+      method: "GET",
+      "Access-Control-Allow-Origin": "*",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        this.movies = data;
+      });
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.photo {
+  width: 50px;
+}
+</style>
