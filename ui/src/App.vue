49c38133afc59dfadaf407bdd37b88dd23c38200
yourdrug <mitkojenia@gmail.com>
2023-10-15 21:49:05 +0300

    test djoser endpoints in client

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="app">
     <list-movies v-bind:short_movies="movies"> </list-movies>
+    <button @click="logout">ВЫЙТИ</button>
+    <button @click="login">ВОЙТИ</button>
   </div>
 </template>
 
@@ -16,13 +18,14 @@ export default {
   data() {
     return {
       movies: [],
+      user: {},
     };
   },
 
   methods: {
     async fetchMovies() {
       try {
-        const response = await axios.get("http://127.0.0.1:8000/movie/");
+        const response = await axios.get("http://127.0.0.1:8000/movie");
         this.movies = response.data;
       } catch (error) {
         alert("erorr");
@@ -31,10 +34,62 @@ export default {
 
     async fetchShortMovies() {
       try {
-        const response = await axios.get("http://127.0.0.1:8000/movie_short/");
+        const response = await axios.get("http://127.0.0.1:8000/movie_short");
         this.movies = response.data;
       } catch (error) {
-        alert("erorr");
+        alert(error.message);
+      }
+    },
+
+    async login() {
+      let data = { username: "admin", password: "admin" };
+      try {
+        const response = await axios.post(
+          "http://127.0.0.1:8000/auth/token/login",
+          data
+        );
+        localStorage.setItem("token", response.data.auth_token);
+        this.getAccountInfo();
+        alert("Token " + localStorage.getItem("token"));
+      } catch (error) {
+        alert(error.message);
+      }
+    },
+
+    async logout() {
+      let config = {
+        headers: {
+          Authorization: "Token " + localStorage.getItem("token"),
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      };
+      try {
+        const response = await axios.post(
+          "http://127.0.0.1:8000/auth/token/logout",
+          config
+        );
+        alert(response.data);
+      } catch (error) {
+        alert("ошибка в логауте");
+      }
+    },
+
+    async getAccountInfo() {
+      let config = {
+        headers: {
+          Authorization: "Token " + localStorage.getItem("token"),
+        },
+      };
+      try {
+        const response = await axios.get(
+          "http://127.0.0.1:8000/auth/users/me/",
+          config
+        );
+        this.user = response.data;
+        alert(this.user.username);
+      } catch (error) {
+        alert("ошибка туть");
       }
     },
   },
