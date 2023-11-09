9bf0bf64c3a293b35b86cab36fbbbd6802e7bffb
yourdrug <mitkojenia@gmail.com>
2023-11-09 20:10:55 +0300

    refactor class about postgres connection(add pools)

--- movies/movies_database/movie_db.py
+++ movies/movies_database/movie_db.py
@@ -1,13 +1,34 @@
 import psycopg2
+from psycopg2 import pool
 from dotenv import dotenv_values
+from contextlib import contextmanager
 
 config = dotenv_values(".env")
 
 
 class MovieDb:
     def __init__(self):
-        self.conn = psycopg2.connect(database=config["POSTGRES_DB"], user=config["POSTGRES_USER"], host=config["HOST"],
-                                     port=config["PORT"], password=config["POSTGRES_PASSWORD"])
+        db_config = {
+            'database': config["POSTGRES_DB"],
+            'user': config["POSTGRES_USER"],
+            'host': config["HOST"],
+            'port': config["PORT"],
+            'password': config["POSTGRES_PASSWORD"]
+        }
+        self.db_pool = psycopg2.pool.SimpleConnectionPool(1, 20, **db_config)
 
-    def get_cursor(self):
-        return self.conn.cursor()
+    @contextmanager
+    def connect(self):
+        conn = self.db_pool.getconn()
+        try:
+            yield conn
+            conn.commit()
+        finally:
+            conn.close()
+
+    def execute_query(self, query: str):
+        with self.connect() as connection:
+            cursor = connection.cursor()
+            cursor.execute(query)
+            data = cursor.fetchall()
+            return data
