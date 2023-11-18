29413e4797978641930fcb731773e1f65eb2efe9
yourdrug <mitkojenia@gmail.com>
2023-11-18 22:50:44 +0300

    add pagination & fixed view for movie

--- movies/movies_database/views.py
+++ movies/movies_database/views.py
@@ -2,7 +2,6 @@ import random
 
 from django.core.cache import cache
 from django.db.models import Count, Case, When
-from django.shortcuts import redirect
 from django.utils.decorators import method_decorator
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
@@ -11,6 +10,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.mixins import UpdateModelMixin
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
@@ -54,34 +54,47 @@ class MovieViewSet(ModelViewSet):
 
     @action(detail=False, methods=['POST'])
     def addMovies(self, request):
-        movie_temp = Movie.objects.create(
-            name=request.data.get("movies").get("name"),
-            description=request.data.get("movies").get("description"),
-            year=request.data.get("movies").get("year"),
-            watch_time=request.data.get("movies").get("movieLength"),
-            country=request.data.get("movies").get("countries")[0].get("name"),
-            tagline=request.data.get("movies").get("slogan"),
-            poster=request.data.get("movies").get("poster").get("url"),
-            world_premier=request.data.get("movies").get("premiere").get("world").split("T")[0]
-        )
-        for genre in request.data.get("movies").get("genres"):
-            if Genre.objects.filter(name=genre.get("name")).exists():
-                movie_temp.genres.add(Genre.objects.get(name=genre.get("name")))
-            else:
-                temp_genre = Genre.objects.create(name=genre.get("name"), slug=genre.get("name"))
-                movie_temp.genres.add(temp_genre)
+        if not Movie.objects.filter(name=request.data.get("movies").get("name")).exists():
+            max_desc = 1495
+            max_tagline = 195
+            desc = request.data.get("movies").get("description")
+            tagline = request.data.get("movies").get("slogan")
+            points = "..."
+            short_tag = "-"
+            short_desc = "-"
+            if desc is not None:
+                short_desc = desc[:max_desc] + (points if len(desc) > max_desc else "")
+            if tagline is not None:
+                short_tag = tagline[:max_tagline] + (points if len(tagline) > max_tagline else "")
 
-        for person in request.data.get("movies").get("persons"):
-            if Person.objects.filter(name=person.get("name")).exists():
-                temp_person = Person.objects.get(name=person.get("name"))
-            else:
-                temp_person = Person.objects.create(
-                    name=person.get("name"),
-                    photo=person.get("photo"),
-                    en_name=person.get("enName"),
-                )
-            Profession.objects.create(person=temp_person, movie=movie_temp,
-                                      name=person.get("profession"), slug=person.get("enProfession"))
+            movie_temp = Movie.objects.create(
+                name=request.data.get("movies").get("name"),
+                description=short_desc,
+                year=request.data.get("movies").get("year"),
+                watch_time=request.data.get("movies").get("movieLength"),
+                country=request.data.get("movies").get("countries")[0].get("name"),
+                tagline=short_tag,
+                poster=request.data.get("movies").get("poster").get("url"),
+                world_premier=request.data.get("movies").get("premiere").get("world").split("T")[0]
+            )
+            for genre in request.data.get("movies").get("genres"):
+                if Genre.objects.filter(name=genre.get("name")).exists():
+                    movie_temp.genres.add(Genre.objects.get(name=genre.get("name")))
+                else:
+                    temp_genre = Genre.objects.create(name=genre.get("name"), slug=genre.get("name"))
+                    movie_temp.genres.add(temp_genre)
+
+            for person in request.data.get("movies").get("persons"):
+                if Person.objects.filter(name=person.get("name")).exists():
+                    temp_person = Person.objects.get(name=person.get("name"))
+                else:
+                    temp_person = Person.objects.create(
+                        name=person.get("name"),
+                        photo=person.get("photo"),
+                        en_name=person.get("enName"),
+                    )
+                Profession.objects.create(person=temp_person, movie=movie_temp,
+                                          name=person.get("profession"), slug=person.get("enProfession"))
 
         return Response(status=status.HTTP_201_CREATED)
 
@@ -94,7 +107,6 @@ class MovieViewSet(ModelViewSet):
         random_movie = data.get(id=random_id.get("id"))
         serializer = self.get_serializer(random_movie)
         return Response(serializer.data)
-        # return redirect('http://127.0.0.1:8000/movie/' + str(random_id.get("id")))
 
 
 class UserMovieRelationViews(UpdateModelMixin, GenericViewSet):
@@ -110,15 +122,26 @@ class UserMovieRelationViews(UpdateModelMixin, GenericViewSet):
         return obj
 
 
+class MoviesPagination(PageNumberPagination):
+    page_size = 100
+    page_size_query_param = 'page_size'
+    max_page_size = 200
+
+
 class ShortInfoMovieViewSet(ModelViewSet):
     queryset = Movie.objects.all().annotate(
         annotated_count_rate=Count(Case(When(usermovierelation__rate__isnull=False, then=1)))
-    ).prefetch_related('profession_set__person', 'genres').order_by('-world_premier')
+    ).prefetch_related('profession_set__person', 'genres').order_by('id')
     serializer_class = ShortInfoMovieSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['year', 'name', 'genres__name']
     permission_classes = [IsOwnerOrStaffOrReadOnly]
     authentication_classes = (TokenAuthentication,)
+    pagination_class = MoviesPagination
+
+    @method_decorator(cache_page(60 * 2))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
 
 class PersonInfoViewSet(ModelViewSet):
