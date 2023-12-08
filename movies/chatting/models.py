b1b3a717192b420730633a6479b9b2786a179159
yourdrug <mitkojenia@gmail.com>
2023-12-08 10:51:54 +0300

    add str method for models

--- movies/chatting/models.py
+++ movies/chatting/models.py
@@ -9,6 +9,9 @@ class ChatRoom(models.Model):
     type = models.CharField(max_length=10, default='DM')
     users = models.ManyToManyField(User, related_name='user_chats')
 
+    def __str__(self):
+        return f'{self.name} --> {self.type}'
+
 
 class Message(models.Model):
     text = models.TextField(null=False)
@@ -18,5 +21,5 @@ class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_messages')
     chat = models.ForeignKey(ChatRoom, on_delete=models.CASCADE, related_name='chat_messages')
 
-    def str(self):
-        return f'{self.sender}'
+    def __str__(self):
+        return f'{self.sender} --> {self.text}'
